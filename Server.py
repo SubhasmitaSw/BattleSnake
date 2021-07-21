aac61067663ee3132d15107f2ff247fae0b703b3
SubhasmitaSw <subhasmitaswain232@gmail.com>
2021-07-21 21:28:42 +0530

    modifications

--- Server.py
+++ Server.py
@@ -55,6 +55,7 @@ def handle_move():
     # TODO - look at the server_logic.py file to see how we decide what move to return!
     move = server_logic.choose_move(data)
 
+    print(data)
     return {"move": move}
 
 
